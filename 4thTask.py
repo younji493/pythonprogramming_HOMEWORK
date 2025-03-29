495033637d95e3cdb3552af962d54a4fd6637da6
younji493 <tooro9999@gmail.com>
2025-03-29 14:14:36 +0900

    final

--- 4thTask.py
+++ 4thTask.py
@@ -4,7 +4,7 @@ result = int(input("1. 입력한 수식 계산  2. 두 수 사이의 합계 :"))
 if result == 1:
     expr = input("*** 수식을 입력하세요 : ")
     answer = eval(expr)
-    print("%s 결과는 %f입니다."%(expr, answer))
+    print("%s 결과는 %5.1f입니다."%(expr, answer))
 
 elif result == 2:
     a = int(input("첫 번째 숫자를 입력하세요: "))
